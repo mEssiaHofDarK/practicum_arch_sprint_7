ec66a41e0a000cff9b2683f0f868c455f392214c
Your Name <you@example.com>
2025-09-07 06:44:32 +0300

    task 3

---
+++ task_3/build_index.py
@@ -0,0 +1,24 @@
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader
+
+
+def main():
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_storage = Chroma(
+        collection_name="my_d2_collection",
+        embedding_function=embeddings,
+        persist_directory="./chroma_langchain_my_d2_collection_db",
+    )
+    loader = DirectoryLoader("../task_2/knowledge_base", glob="**/*.txt", show_progress=True)
+    docs = loader.load()
+
+    all_splits = text_splitter.split_documents(docs)
+
+    vector_storage.add_documents(documents=all_splits)
+
+
+if __name__ == "__main__":
+    main()
